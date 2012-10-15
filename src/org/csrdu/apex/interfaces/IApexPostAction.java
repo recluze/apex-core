cb2ce5749bb48c62d34031666ab27cc0643afc01
recluze <recluze@gmail.com>
2012-10-15 11:31:16 +0500

    Added interface for post actions

---
+++ src/org/csrdu/apex/interfaces/IApexPostAction.java
@@ -0,0 +1,18 @@
+/* 
+	Part of the Apex Framework. 
+	
+	Provided under GPLv2. 
+	
+	author: Nauman (nauman@csrdu.org) 
+			http://csrdu.org/nauman 
+
+*/ 
+package org.csrdu.apex.interfaces;
+
+import java.util.Vector;
+
+public interface IApexPostAction {
+
+	public Boolean perform(Vector<Object> params); 
+	
+}
